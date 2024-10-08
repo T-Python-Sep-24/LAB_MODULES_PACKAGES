012de6c6dcfde979dec6971f4c361dfba9c1b1f8
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-08 13:48:59 +0300

    This is my answer for Lab 10

---
+++ dateOP.py
@@ -0,0 +1,10 @@
+#Lab 10
+
+#import from python library
+
+import datetime
+
+def current_date():
+    return datetime.date.today()
+
+print(f"Your Current Date is: {current_date()}")
---
+++ main.py
@@ -0,0 +1,7 @@
+#main file to call current date
+
+import dateOP
+
+#call the function that print the current date
+dateOP.current_date()
+print(f"This is to test the main python file: {dateOP.current_date()} **")
