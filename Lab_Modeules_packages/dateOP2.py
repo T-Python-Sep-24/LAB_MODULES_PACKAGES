1af18992c004db7afe3fd39ac5d264378d0c0398
MarwahDesigner90 <marwaty666@gmail.com>
2024-10-10 10:11:57 +0300

    The task of modules packages  lab1 assignment has been solved

---
+++ Lab_Modeules_packages/dateOP2.py
@@ -0,0 +1,5 @@
+import datetime
+
+def print_current_date():
+   day_date = datetime.date.today()
+   print("Current date:", day_date)
---
+++ Lab_Modeules_packages/main.py
@@ -0,0 +1,4 @@
+
+import Lab_Modeules_packages.dateOP2 as date
+
+date.print_current_date()
